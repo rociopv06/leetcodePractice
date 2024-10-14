48014d1bb040f3d7a0ad2d608c07bea4a143a590
Rocio PV <61081323+rociopv06@users.noreply.github.com>
2024-10-14 00:02:21 -0400

    Create LeetCodeM2.1.java

---
+++ LeetCodeM2.1.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/find-peak-element/description/
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        if(nums.length == 1) return 0;
+        while (left < right) {
+            int mid = left + (right-left)/2;
+            if (nums[mid] > nums[mid+1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
